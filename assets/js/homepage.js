2ef19044e21a3a19c831a3212ef8fd68c044349d
viktorzhou <zhou_dongliang@icloud.com>
2026-07-18 13:04:12 +0800

    update

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -8,25 +8,30 @@ var _hmt = window._hmt || [];
   firstScript.parentNode.insertBefore(script, firstScript);
 }());
 
-window.dataLayer = window.dataLayer || [];
-
-function gtag() {
-  window.dataLayer.push(arguments);
-}
-
-gtag("js", new Date());
-gtag("config", "UA-159825357-1");
-
 document.addEventListener("DOMContentLoaded", function loadPublications() {
-  fetch("./publications.html")
+  var publicationsList = document.getElementById("publications-list");
+  var isChinese = document.documentElement.lang.toLowerCase().startsWith("zh");
+
+  if (!publicationsList) {
+    return;
+  }
+
+  fetch("./publications.html", { cache: "no-cache" })
     .then(function (response) {
+      if (!response.ok) {
+        throw new Error("Unable to load publications: HTTP " + response.status);
+      }
+
       return response.text();
     })
     .then(function (html) {
-      var publicationsList = document.getElementById("publications-list");
-
-      if (publicationsList) {
-        publicationsList.innerHTML = html;
-      }
+      publicationsList.innerHTML = html;
+    })
+    .catch(function (error) {
+      publicationsList.innerHTML = "<p class=\"publication-status publication-error\" role=\"alert\">" +
+        (isChinese ? "论文列表加载失败，请稍后刷新页面重试。" :
+          "Publications could not be loaded. Please refresh the page and try again.") +
+        "</p>";
+      console.error(error);
     });
 });
